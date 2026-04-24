4f9462dd384efceb218977468c9de9cfdb07ea62
mynordma <mynordma@gmail.com>
2026-04-24 14:58:30 -0600

    funcion hash

--- GestorSucursales/src/main/java/com/mycompany/gestorsucursales/edd/tablahash/TablaHash.java
+++ GestorSucursales/src/main/java/com/mycompany/gestorsucursales/edd/tablahash/TablaHash.java
@@ -4,13 +4,51 @@
  */
 package com.mycompany.gestorsucursales.edd.tablahash;
 
+import com.mycompany.gestorsucursales.modelos.Producto;
+
 /**
  *
  * @author mynordma
  */
 public class TablaHash {
     
-    private String h(String k){
-        return k;
+    private Producto[] t;
+    private double FC;
+    private final double FC_IDEAL = 0.8;
+    private final double R = 0.6180334;
+    private int n;
+    private int m;
+    
+    public TablaHash(){
+        m = 16;
+        t = new Producto[m];
+        n = 0;
+        FC = n / m;
+    }
+    
+    /**
+     * Función hash con método de la multiplicación
+     * @param x
+     * @return indice de x en t
+     */
+    private int h(int x) {
+        double rx = x * R;
+        double d = rx - Math.floor(rx);
+        return (int)(m * d);
+    }
+    
+    public Producto buscar(Producto p){
+        int x = Integer.parseInt(p.getCodigoBarras());
+        return t[h(x)];
+    }
+    
+    public void insertar(Producto p){
+        int x = Integer.parseInt(p.getCodigoBarras());
+        t[h(x)] = p;
+    }
+    
+    public void eliminar(Producto p){
+        int x = Integer.parseInt(p.getCodigoBarras());
+        t[h(x)] = null;
     }
 }
